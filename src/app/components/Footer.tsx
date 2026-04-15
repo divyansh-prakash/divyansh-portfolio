c0d768bd4d8451438a60d3c16117a42bd0ff11c3
divyansh-prakash <divyanshlko.1999@gmail.com>
2026-04-15 19:41:12 +0530

    Footer fix

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -126,7 +126,7 @@ const Footer = () => (
         </p>
         <p className="flex items-center gap-2">
           Made with <span className="text-red-600 animate-pulse">❤️</span> in
-          Pakistan
+          India
         </p>
       </div>
     </div>
